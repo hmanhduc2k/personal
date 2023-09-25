084d7c1c4099e63ed6174f47b202220d42e5b073
hmanhduc2k <hmanhduc2k@gmail.com>
2023-09-25 23:27:50 +0800

    add file skeletons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  navBar,
+  mainBody,
+  about,
+  repos,
+  leadership,
+  skills,
+  getInTouch,
+  experiences
+} from "./editable-stuff/config.js";
+import MainBody from "./components/home/MainBody";
+import AboutMe from "./components/home/AboutMe";
+import Project from "./components/home/Project";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Skills from "./components/home/Skills";
+// import { Blog } from "./components/blog/Blog";
+// import BlogPost from "./components/blog/BlogPost";
+import GetInTouch from "./components/home/GetInTouch.jsx";
+import Leadership from "./components/home/Leadership.jsx";
+import Experience from "./components/home/Experience";
+
 function App() {
   return (
     <div className="App">
@@ -19,6 +42,7 @@ function App() {
         </a>
       </header>
     </div>
+
   );
 }
 
